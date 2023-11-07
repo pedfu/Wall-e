13b7cbcb45b015b03ef022e17e2d6b35dc9a1971
Pedro Fuziwara <fuziwarapedro@gmail.com>
2023-11-07 07:59:17 -0300

    feat: Add modal create post

---
+++ client/src/components/ModalPost.jsx
@@ -0,0 +1,61 @@
+import { useCallback, useState } from 'react'
+import PropTypes from 'prop-types'
+import Modal from './Modal'
+import Button from './Button'
+
+const ModalPost = ({ onSubmit, onClose, prompt, image, isLoading }) => {
+    const [text, setText] = useState('')
+
+    const onTextChange = useCallback(event => {
+        const { value } = event.target
+        setText(value)
+    }, [])
+
+  return (
+    <Modal onClose={() => {
+        console.log('2') 
+        onClose()
+        }} className='bg-grey rounded-xl min-w-[600px] max-w-[1000px] text-white'>
+        {isLoading ? (
+            <div className='w-full h-full min-h-[400px] flex items-center justify-center'>
+                <svg className='animate-spin' width="30px" height="30px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><path fillRule="evenodd" clipRule="evenodd" d="M13.917 7A6.002 6.002 0 0 0 2.083 7H1.071a7.002 7.002 0 0 1 13.858 0h-1.012z"/></svg>
+            </div>
+        ) : (
+            <>
+                <h1 className='text-lg m-2'>Create post</h1>
+                <div className='mt-1 min-h-[400px] flex justify-between'>
+                    <img src={image} className='w-[50%] m-2' />
+                    <div className='w-[50%] m-2'>
+                        <label>
+                            <p className='w-[calc(100%-8px)] font-light'>Prompt</p>
+                            <input className='w-[calc(100%-8px)] rounded-md p-1 text-sm font-light mt-1 mb-4' disabled value={prompt} />
+                        </label>
+
+                        <textarea className='mt-1 rounded-md w-[calc(100%-8px)] text-sm font-light bg-fontGrey p-1 resize-none' rows={8} onChange={onTextChange} value={text} placeholder='Share something with others' />
+
+                        <p className='font-light text-sm mt-4 w-[calc(100%-8px)] text-center'>The comments wills be shown here</p>
+
+                        <div className='flex justify-end w-[calc(100%-8px)] items-end'>
+                            <Button onClick={() => onSubmit(text)}>Post</Button>                    
+                        </div>
+                    </div>
+                </div>
+            </>
+            )}
+    </Modal>
+  )
+}
+
+ModalPost.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired,
+    prompt: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    isLoading: PropTypes.bool,
+}
+
+ModalPost.defaultProps = {
+    isLoading: false
+}
+
+export default ModalPost
